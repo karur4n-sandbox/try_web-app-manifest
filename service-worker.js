93369d30f2950d6883f4d110b00bff047e087836
karur4n <karuran.f@gmail.com>
2018-11-06 00:00:04 +0900

    Chrome で A2HS すると「Site cannot be installed: the page does not work offline」エラー対策

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', function (event) {
     console.log('installing')
 })
+
+self.addEventListener('fetch', function (event) {
+    // 「Site cannot be installed: the page does not work offline」対策に空のリスナー
+    // 参考： https://amymd.hatenablog.com/entry/2017/10/12/001612
+})
